2e3b4cb3bc8e337b38344d80b2a97faa2a3703e3
Keisokki <keisokki_wijaya_33rpl@student.smktelkom-mlg.sch.id>
2025-07-29 14:18:45 +0700

    Add files via upload

---
+++ Ekskul AI/pertemuan_2.py
@@ -0,0 +1,13 @@
+'''Bedanya dengan .py, .ipynb bisa di run langsung sekaligus satu file seperti Jupyter, tidak harus satu satu baris'''
+print('hello world')
+
+
+print("I can't sleep")
+#Beda sama
+print('I can't sleep')
+      
+
+nama = "Shabri"
+umur = 16
+print("Namaku {} umur {}".format(nama, umur))
+print("Namaku {} umur {}".format("Shabri", 16))
